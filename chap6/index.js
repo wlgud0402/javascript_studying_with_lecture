31629bd1b86dfd899e8073a781ff37a32d37e0df
wlgud0402 <wlgudrlgus@naver.com>
2020-10-01 23:55:19 +0900

    game project

--- chap6/index.js
+++ chap6/index.js
@@ -1,3 +1,19 @@
 function start_game() {
   console.log("hello");
+  position_setting();
 }
+
+function position_setting() {
+  let top = Math.floor(Math.random() * 201) + 250;
+  let right = Math.floor(Math.random() * 751) + 50;
+}
+
+const x = event.clientX;
+const y = event.clientY;
+console.log(x, y);
+
+vertical.style.left = `${x}px`;
+horizontal.style.top = `${y}px`;
+
+target.style.left = `${x}px`;
+target.style.top = `${y}px`;
